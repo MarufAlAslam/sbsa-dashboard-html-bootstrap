44a9d0cf1496e2cf87b21d4e198cb3c8a4beafd6
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-16 14:11:14 +0600

    calendar updated

--- js/app.js
+++ js/app.js
@@ -179,4 +179,22 @@ function closeAllSelect(elmnt) {
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
 
-$('input[name="dates"]').daterangepicker();
+$('input[name="dates"]').daterangepicker({
+  opens: "right",
+  autoUpdateInput: true,
+  ranges: {
+    Today: [moment(), moment()],
+    Yesterday: [
+      moment().subtract(1, "days"),
+      moment().subtract(1, "days"),
+    ],
+    "Last 7 Days": [moment().subtract(6, "days"), moment()],
+    "Last 30 Days": [moment().subtract(29, "days"), moment()],
+    "This Month": [moment().startOf("month"), moment().endOf("month")],
+    "Last Month": [
+      moment().subtract(1, "month").startOf("month"),
+      moment().subtract(1, "month").endOf("month"),
+    ],
+  },
+  alwaysShowCalendars: true,
+});
