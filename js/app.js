782793a4aefad8ef3832ad874ef11f82a0092c44
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-22 17:30:09 +0600

    overflow removed

--- js/app.js
+++ js/app.js
@@ -197,33 +197,33 @@ $('input[name="dates"]').daterangepicker({
 });
 
 // make overflow-x-auto draggable to scroll
-const slider = document.querySelector(".overflow-x-auto");
-let isDown = false;
-let startX;
-let scrollLeft;
+// const slider = document.querySelector(".overflow-x-auto");
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  slider.classList.add("active");
-  slider.style.cursor = "grabbing";
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.style.cursor = "pointer";
-  slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.style.cursor = "pointer";
-  slider.classList.remove("active");
-});
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
-});
+// slider.addEventListener("mousedown", (e) => {
+//   isDown = true;
+//   slider.classList.add("active");
+//   slider.style.cursor = "grabbing";
+//   startX = e.pageX - slider.offsetLeft;
+//   scrollLeft = slider.scrollLeft;
+// });
+// slider.addEventListener("mouseleave", () => {
+//   isDown = false;
+//   slider.style.cursor = "pointer";
+//   slider.classList.remove("active");
+// });
+// slider.addEventListener("mouseup", () => {
+//   isDown = false;
+//   slider.style.cursor = "pointer";
+//   slider.classList.remove("active");
+// });
+// slider.addEventListener("mousemove", (e) => {
+//   if (!isDown) return;
+//   e.preventDefault();
+//   const x = e.pageX - slider.offsetLeft;
+//   const walk = (x - startX) * 3; //scroll-fast
+//   slider.scrollLeft = scrollLeft - walk;
+//   console.log(walk);
+// });
