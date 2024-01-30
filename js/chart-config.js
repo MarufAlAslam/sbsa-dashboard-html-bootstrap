43d45dd8defd6a4e5f6adb0d144a8978c3243d25
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-30 14:09:38 +0600

    below chart added

--- js/chart-config.js
+++ js/chart-config.js
@@ -85,3 +85,70 @@ const myRadarChart = new Chart("radarChart", {
     },
   },
 });
+
+// big chart
+var bigChartOptions = {
+  series: [
+    {
+      name: "Amazon",
+      data: [
+        105, 90, 75, 60, 60, 60, 60, 59, 59, 59, 59, 59, 59, 59, 58, 58, 58, 58,
+      ],
+    },
+    {
+      name: "Grainger",
+      data: [
+        455, 390, 325, 260, 259, 259, 258, 257, 257, 256, 255, 255, 254, 254,
+        253, 252, 252, 251,
+      ],
+    },
+    {
+      name: "Chewy",
+      data: [
+        105, 90, 75, 60, 60, 60, 60, 59, 59, 59, 59, 59, 59, 59, 58, 58, 58, 58,
+      ],
+    },
+    {
+      name: "Walmart",
+      data: [
+        70, 60, 50, 40, 39.9, 39.8, 39.7, 39.6, 39.5, 39.4, 39.3, 39.2, 39.1,
+        39, 38.9, 38.8, 38.7, 38.6,
+      ],
+    },
+  ],
+  chart: {
+    type: "area",
+    height: 400,
+    stacked: true,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#2B7F75", "#FF9F38", "#84818A", "#FFC000"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      opacityFrom: 1,
+      opacityTo: 1,
+    },
+  },
+  legend: {
+    position: "top",
+    horizontalAlign: "right",
+  },
+  xaxis: {
+    type: "string",
+  },
+};
+
+var bigChart = new ApexCharts(
+  document.querySelector("#bigChart"),
+  bigChartOptions
+);
+bigChart.render();
