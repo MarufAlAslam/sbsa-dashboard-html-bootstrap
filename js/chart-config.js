a3d6841960ac10f53909a4e2bc59a0fba8605ef8
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-04-04 21:40:23 +0600

    colors added

--- js/chart-config.js
+++ js/chart-config.js
@@ -257,7 +257,26 @@ var fatChartOptions = {
     with: "100%",
     type: "treemap",
   },
-  colors: ["#2B7F75", "#FFC000", "#84818A", "#FFC000"],
+  colors: [
+    '#3B93A5',
+    '#F7B844',
+    '#ADD8C7',
+    '#EC3C65',
+    '#CDD7B6',
+    '#C1F666',
+    '#D43F97',
+    '#1E5D8C',
+    '#421243',
+    '#7F94B0',
+    '#EF6537',
+    '#C0ADDB'
+  ],
+  plotOptions: {
+    treemap: {
+      distributed: true,
+      enableShades: false
+    }
+  },
   // hide the download button
   toolbar: {
     show: false,
